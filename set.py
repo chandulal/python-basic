3b90bb7a1bbfb50f898bbc51f8d25e977de6504d
chandulal.kavar <cckavar@gmail.com>
2018-08-03 15:07:43 +0800

    tuple and set operations

---
+++ set.py
@@ -0,0 +1,4 @@
+stringset = set(("apple", "apple", "banana"))
+print(stringset)
+stringset.add("orange")
+print(stringset)
---
+++ tuple.py
@@ -0,0 +1,4 @@
+stringtuple = (1,2,3)
+print(type(stringtuple))
+print(stringtuple)
+print(len(stringtuple))
