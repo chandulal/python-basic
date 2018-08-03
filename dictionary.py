c56a76cb810c2ab2a9fc2315dca98f33ea48c4b4
chandulal.kavar <cckavar@gmail.com>
2018-08-03 15:27:55 +0800

    dictionary and if conditions

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+thisdict = {
+"apple" : "green",
+"banana":"yellow",
+"cherry":"red"
+}
+print(thisdict)
+thisdict["apple"] = "red"
+print(thisdict)
+dict2 = dict(apple="green", banana="yellow")
+print(dict2)
+del(dict2["apple"])
+print(dict2)
---
+++ ifcond.py
@@ -0,0 +1,9 @@
+a = 33
+b = 100
+if b>a: print("b is greater than a")
+if b>a:
+ print("b is greater than b")
+elif a == b:
+ print("a and b are equal")
+else:
+ print("a is greater than b")
